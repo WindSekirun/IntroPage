e4929b59b9143b4b8eccc1ee4b6d9c94c5c02d24
Pyxis <pyxis@uzuki.live>
2021-01-24 02:55:21 +0900

    add github, stackoverflow

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,40 +1,53 @@
 <template>
-  <v-container id="main">
-    <v-row justify="space-around">
-      <v-avatar size="175">
-        <img
-          src="../assets/profile_akari_square.png"
-          alt="WindSekirun"
-        />
-      </v-avatar>
-    </v-row>
-    <v-row class="text-center">
-      <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          WindSekirun (wind.seo)
-        </h1>
-        <h3 class="display-1 font-weight-light mb-3">
-          Kotlin Lover, Android Devloper
-        </h3>
-      </v-col>
-      <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          Contact Me
-        </h2>
-        <br>
-        <v-row justify="center">
-          <a
-            v-for="(contact, i) in contact"
-            :key="i"
-            :href="contact.link"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            <font-awesome-icon :icon="[contact.iconkind || 'fab', contact.icon]" size="3x" />
-            <p id="platform">{{ contact.platform }}</p>
-          </a>
-        </v-row>
-      </v-col>
+  <v-container id="main" fill-height class="x-ctn">
+    <v-row align="center" justify="center">
+      <v-row justify="space-around">
+        <v-avatar size="225">
+          <img src="../assets/profile_akari_square.png" alt="WindSekirun" />
+        </v-avatar>
+      </v-row>
+      <v-row class="text-center">
+        <v-col class="mb-1" id="name">
+          <h1 class="display-2 font-weight-bold mb-2">
+            WindSekirun (wind.seo)
+          </h1>
+          <h3 class="display-1 font-weight-light mb-1">
+            Kotlin Lover, Android Devloper
+          </h3>
+          <h3 class="font-weight-light">
+            Current working on
+            <a href="https://github.com/kakaobank">@kakaobank</a>
+          </h3>
+        </v-col>
+        <v-col cols="12">
+          <h2 class="headline font-weight-bold mb-3">
+            Contact Me
+          </h2>
+        </v-col>
+        <v-col class="mb-3" cols="12">
+          <v-row justify="center">
+            <a
+              v-for="(contact, i) in contact"
+              :key="i"
+              :href="contact.link"
+              class="subheading mx-3"
+            >
+              <v-tooltip top>
+                <template v-slot:activator="{ on, attrs }">
+                  <font-awesome-icon
+                    :icon="[contact.iconkind || 'fab', contact.icon]"
+                    size="3x"
+                    v-bind="attrs"
+                    v-on="on"
+                  />
+                </template>
+                <span>{{ contact.tooltip }}</span>
+              </v-tooltip>
+              <p id="platform">{{ contact.platform }}</p>
+            </a>
+          </v-row>
+        </v-col>
+      </v-row>
     </v-row>
   </v-container>
 </template>
@@ -46,28 +59,52 @@ export default {
   data: () => ({
     contact: [
       {
+        platform: "Github",
+        link: "https://github.com/WindSekirun/",
+        icon: "github",
+        tooltip: "@WindSekirun",
+      },
+      {
+        platform: "StackOverflow",
+        link: "https://stackoverflow.com/users/1882568/windsekirun",
+        icon: "stack-overflow",
+        tooltip: "WindSekirun",
+      },
+      {
+        platform: "KakaoTalk",
+        link: "/",
+        icon: "comment",
+        tooltip: "WindSekirun",
+        iconkind: "fas",
+      },
+      {
         platform: "Telegram",
         link: "https://t.me/windsekirun",
+        tooltip: "@WindSekirun",
         icon: "telegram-plane",
       },
       {
         platform: "Twitter",
         link: "https://twitter.com/WindSekirun/",
+        tooltip: "@WindSekirun",
         icon: "twitter",
       },
       {
         platform: "Discord",
         link: "https://discordapp.com/users/393392699329478657",
+        tooltip: "Pyxis#1324",
         icon: "discord",
       },
       {
         platform: "LinkedIn",
         link: "https://www.linkedin.com/in/windsekirun/",
+        tooltip: "Seo Dong Gil",
         icon: "linkedin",
       },
       {
         platform: "Blog",
         link: "https://pyxispub.uzuki.live/",
+        tooltip: "WindSekirun",
         icon: "blog",
         iconkind: "fas",
       },
@@ -77,9 +114,6 @@ export default {
 </script>
 
 <style>
-#main {
-  margin-top: 60px;
-}
 a {
   text-decoration: none;
   color: #252525;
--- src/main.js
+++ src/main.js
@@ -2,18 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faBlog} from '@fortawesome/free-solid-svg-icons'
+import { faBlog, faComment } from '@fortawesome/free-solid-svg-icons'
 // import { faComment } from '@fortawesome/free-regular-svg-icons'
-import { faTwitter, faTelegramPlane, faDiscord, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { faTwitter, faTelegramPlane, faDiscord, faLinkedin, faGithub, faStackOverflow } from '@fortawesome/free-brands-svg-icons'
 import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText } from '@fortawesome/vue-fontawesome'
 
 
 library.add(
+  faGithub,
   faTwitter, 
   faTelegramPlane,
   faDiscord,
   faLinkedin,
-  faBlog
+  faBlog,
+  faComment,
+  faStackOverflow
 )
 
 Vue.component('font-awesome-layers', FontAwesomeLayers)
